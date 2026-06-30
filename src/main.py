7f0a0e199914ccbb02ebdee876e1b75b063abc5c
CiceroDaniel <santosemanuelly997@gmail.com>
2026-06-30 10:31:18 -0300

    Iniciando o trabalho de calculo numerico, turma 2026.01

---
+++ src/main.py
@@ -0,0 +1,11 @@
+'''
+TRABALHO DE CALCULO NUMÉRICO - 2026.1
+ALUNO: CÍCERO DANIEL BARBOSA DOS SANTOS
+PROFESSOR:
+'''
+
+'''
+OBJETIVO: Implemetntar o metodo da falsa posicao para encontrar a raiz de uma função.
+'''
+
+print("TRABALHO DE CALCULO NUMÉRICO - 2026.1")
