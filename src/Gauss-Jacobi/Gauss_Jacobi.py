03b34071717a7a2e7149874755dd785c81d030b4
CiceroDaniel <santosemanuelly997@gmail.com>
2026-07-11 19:34:06 -0300

    Iniciado o metodo de Gauss-Jacobi

---
+++ src/Gauss-Jacobi/Gauss_Jacobi.py
@@ -0,0 +1,35 @@
+# OBJETIVO: Implementar em python o metodo de Gauss-Jacobi
+
+# BIBLIOTECAS
+
+import numpy as np
+
+# VARIÁVEIS 
+
+tolerancia = 1e-4
+
+# SISTEMAS QUE SERÃO USADOS
+
+# Sistema A (4x4)
+
+S = [
+    [10,2,-1],
+    [-3,-6,2],
+    [1,1,5]
+]
+
+b = [27,-61.5,-21,5]
+
+# Sistema B (3x3)
+
+'''
+
+S = [
+    [4,1,2],
+    [3,5,1],
+    [1,1,3]
+]
+
+b = [4,7,3]
+
+'''
