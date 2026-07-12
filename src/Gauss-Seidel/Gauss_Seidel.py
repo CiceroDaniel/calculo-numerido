366291b49a885ce269549587bc15674eec7b515c
CiceroDaniel <santosemanuelly997@gmail.com>
2026-07-12 20:10:06 -0300

    Concluido o método de Gauss-Seidel

--- src/Gauss-Seidel/Gauss_Seidel.py
+++ src/Gauss-Seidel/Gauss_Seidel.py
@@ -36,3 +36,58 @@ b3 = [10, 11, 3]
 
 tolerancia = 1e-4
 max_iteracoes = 50
+
+
+# FUNÇÃO DE GAUSS-JACOBI
+def gauss_seidel(S,b,Tol,max_iteracoes):
+    n = len(S)
+
+    # Aproximações iniciais
+
+    x = [0]*n
+    for k in range(max_iteracoes):
+
+        x_antigo = x.copy()
+
+        for i in range(n):
+            soma = 0
+            for j in range(n):
+                if i != j:
+                    soma += S[i][j]*x[j]
+            x[i] = (b[i] - soma) / S[i][i]
+    
+        # Calcula o erro
+        erro = max(abs(x[i] - x_antigo[i]) for i in range(n))
+
+        if n == 4:
+            print(
+            f"Iteração {k+1:2d}: "
+            f"x1 = {x[0]:.6f} | "
+            f"x2 = {x[1]:.6f} | "
+            f"x3 = {x[2]:.6f} | "
+            f"x4 = {x[3]:.6f}"
+            )
+        elif n == 3:
+            print(
+            f"Iteração {k+1:2d}: "
+            f"x1 = {x[0]:.6f} | "
+            f"x2 = {x[1]:.6f} | "
+            f"x3 = {x[2]:.6f} | "
+            )
+
+        # Condição de parada
+        if erro < Tol:
+            print(f"\nConvergiu em {k+1} iterações")
+            return x, k+1, True
+        
+    print("\nO método não convergiu.")
+    print(f"Máximo de {max_iteracoes} iterações atingido.")
+
+    return x, max_iteracoes, False
+
+resolucao,iteracoes,convergiu = gauss_seidel(A,b1,tolerancia,max_iteracoes)
+
+if convergiu:
+    print("Solução:", resolucao)
+else:
+    print("Última aproximação:", resolucao)
