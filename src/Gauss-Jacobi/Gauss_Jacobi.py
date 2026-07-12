4483ed31e2d5662f4d4a285198d7248ac6feec97
CiceroDaniel <santosemanuelly997@gmail.com>
2026-07-12 12:14:33 -0300

    Iniciado a função de Gauss-Jacobi

--- src/Gauss-Jacobi/Gauss_Jacobi.py
+++ src/Gauss-Jacobi/Gauss_Jacobi.py
@@ -6,38 +6,40 @@ import numpy as np
 
 # Sistema A (4x4)
 
-S = [
+A = [
     [10,-1,2,0],
     [-1,11,-1,3],
     [2,-1,10,-1],
     [0,3,-1,8]
 ]
 
-b = [6,25,-11,15]
+b1 = [6,25,-11,15]
 
 # Sistema B (3x3)
 
-'''
-
-S = [
+B = [
     [2,8,-1],
     [5,-1,1],
     [-1,1,4]
 ]
 
-b = [11,10,3]
+b2 = [11,10,3]
 
-'''
 
 # VARIÁVEIS 
 
 tolerancia = 1e-4
 max_iteracoes = 50
-n = len(S)
-x = [0]*n
-x_novo = [0]*n
+
 
 # SISTEMAS QUE SERÃO USADOS 
 
+def gauss_jacobi(S,b,Tol,max_interracoes):
+    n = len(S)
 
-print(S)
+    # Aproximações iniciais
+
+    x = [0]*n
+    x_novo = [0]*n
+    
+
